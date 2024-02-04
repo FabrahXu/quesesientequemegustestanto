df04d0ced60334f00ec9d1dc5a79206d9eb21dd6
FabrahXu <158857528+FabrahXu@users.noreply.github.com>
2024-02-04 16:40:33 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesbtn = document.querySelector("#yesbtn");
+
+yesbtn.addEventListener("click", function () {
+    alert("te amo ♡")
+})
+
+const nobtn = document.querySelector("#nobtn");
+
+nobtn.addEventListener("click", function () {
+    alert("te adoro ♡")
+})
